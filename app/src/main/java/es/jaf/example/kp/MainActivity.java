a5daaeb7267b714b19cac1611759e3bf0a06102c
pepeillo <jaferriz@gmail.com>
2021-07-13 20:07:42 +0200

    icono

--- app/src/main/java/es/jaf/example/kp/MainActivity.java
+++ app/src/main/java/es/jaf/example/kp/MainActivity.java
@@ -7,80 +7,19 @@ import android.os.Bundle;
 import android.os.Process;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.biometric.BiometricPrompt;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import es.jaf.example.kp.database.DbManager;
 import net.sqlcipher.database.SQLiteDatabase;
 
-import java.util.concurrent.Executor;
-
 public class MainActivity extends FragmentActivity {
-
-    //UI Views
-    private TextView authStatusTv;
-
-    private BiometricPrompt biometricPrompt;
-    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //init UI views
-        authStatusTv = findViewById(R.id.authStatusTv);
-        View authBtn = findViewById(R.id.authBtn);
-
-        //init bio metric
-
-        Executor executor = ContextCompat.getMainExecutor(this);
-        //error authenticating, stop tasks that requires auth
-        //failed authenticating, stop tasks that requires auth
-        BiometricPrompt.AuthenticationCallback authCB = new BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-                //error authenticating, stop tasks that requires auth
-                authStatusTv.setText(getString(R.string.auth_error) + ": " + errString);
-            }
-
-            @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-
-                findViewById(R.id.layoutPassword).setVisibility(View.VISIBLE);
-                findViewById(R.id.cmdLogin).setVisibility(View.VISIBLE);
-                findViewById(R.id.imgBiometric).setVisibility(View.INVISIBLE);
-                findViewById(R.id.authStatusTv).setVisibility(View.INVISIBLE);
-                findViewById(R.id.authBtn).setVisibility(View.INVISIBLE);
-            }
-
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                //failed authenticating, stop tasks that requires auth
-                authStatusTv.setText(R.string.auth_failed);
-            }
-        };
-        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, authCB);
-
-        //setup title,description on auth dialog
-        promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle(getString(R.string.auth_title))
-                .setNegativeButtonText(getString(android.R.string.cancel))
-                .build();
-
-        //handle authBtn click, start authentication
-        authBtn.setOnClickListener(v -> {
-            //show auth dialog
-            biometricPrompt.authenticate(promptInfo);
-        });
 
         SQLiteDatabase.loadLibs(getApplicationContext());
         findViewById(R.id.cmdLogin).setOnClickListener(view -> {
@@ -99,7 +38,6 @@ public class MainActivity extends FragmentActivity {
                 cmdHideShow.setImageResource(R.drawable.hide);
             }
         });
-
     }
 
     @Override
